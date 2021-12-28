15291df358e27a5c859bda7e4e5eb1386a0a24d8
Maria02179 <73477221+Maria02179@users.noreply.github.com>
2021-12-28 17:27:54 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,61 @@
-// объявление функций и классов для вычисления арифметических выражений
+// РѕР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёР№ Рё РєР»Р°СЃСЃРѕРІ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+#ifndef __TARITHMETIC_H__
+#define __TARITHMETIC_H__
+#include <string>
+#include <vector>
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+const string Symbols = "0123456789.()+-/* ";
+
+struct Lexem
+{
+
+private:
+	char operation;
+	double value;
+	bool type;
+public:
+	Lexem() = default;
+	Lexem(const double &_value)
+	{
+		value = _value;
+		type = true;
+	}
+	Lexem(const char &_operation)
+	{
+		operation = _operation;
+		type = false;
+	}
+	void printLexem()
+	{
+		if (type)
+			std::cout << value << " ";
+		else
+			std::cout << operation << " ";
+	}
+	double getValue() { return value; }
+	char getOperation() { return operation; }
+	bool getType() { return type; }
+	int priority();
+};
+
+class Arithmetic
+{
+public:
+	vector<Lexem> smbls;
+
+
+	Arithmetic() = default;
+	void divide(string &expression);
+	void ToPostfix();
+	double calculate();
+	void print();
+};
+
+bool checkBrackets(const string &s);
+bool checkSymbols(const string &s);
+bool isCorrect(const string &s);
+#endif
