da9fae7740650120585dfbf666061f3b219f9265
Maria02179 <73477221+Maria02179@users.noreply.github.com>
2021-12-28 17:33:05 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,30 @@
 // реализация пользовательского приложения
+#include "../include/arithmetic.h"
+
+using namespace std;
 
 int main()
 {
-  return 0;
+	string str;
+	for (int i = 0; i < 100; i++) {
+		try {
+			cout << "Enter your expression:\n";
+			getline(cin, str);
+			isCorrect(str);
+			Arithmetic res;
+			cout << "Your expression is:\n";
+			res.divide(str);
+			res.print();
+			cout << endl;
+			res.ToPostfix();
+			cout << endl;
+			double result = res.calculate();
+			cout << "Result:\n" << result << endl;
+		}
+		catch (char* smth) {
+			cout << smth << endl;
+		}
+		cout << endl;
+	}
+	return 0;
 }
